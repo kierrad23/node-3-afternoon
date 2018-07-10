1a2ed8243215e8da7e3d8e06071c7845f1dd68fe
Kierra Holmes <Macce@Kierras-MacBook-Pro.local>
2018-07-10 16:23:15 -0500

    finished

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bp = require("body-parser");
+const session = require("express-session");
+require("dotenv").config();
+
+const check = require("./middlewares/checkForSession");
+const swag_controller = require("./controllers/swag_controller");
+const auth_controller = require("./controllers/auth_controller");
+const cart_controller = require("./controllers/cart_controller");
+const search_controller = require("./controllers/search_controller");
+
+const app = express();
+
+app.use(bp.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(check);
+app.use(express.static(`${__dirname}/build`));
+
+app.get("/api/swag", swag_controller.read);
+
+app.post("/api/login", auth_controller.login);
+app.post("/api/register", auth_controller.register);
+app.post("/api/signout", auth_controller.signout);
+app.get("/api/user", auth_controller.getUser);
+
+app.post("/api/cart", cart_controller.add);
+app.post("/api/cart/checkout", cart_controller.checkout);
+app.delete("/api/cart", cart_controller.delete);
+
+app.get("/api/search", search_controller.search);
+
+port = process.env.PORT || 3000;
+app.listen(port, () => console.log(port));
